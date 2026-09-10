1f7d53a3cf2827f5731ec8bf53a13be69662e370
AtwarGroup <github@tiradorstores.com>
2026-09-11 01:56:02 +0300

    Production 1.7E - remove legacy Firebase remnants

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,2 @@
-window.ATWAR_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCgvfHsmtb0qYAMfUjV7iKmZUcJ5n_aSy4",
-  authDomain: "atwarpms.firebaseapp.com",
-  databaseURL: "https://atwarpms-default-rtdb.firebaseio.com",
-  projectId: "atwarpms",
-  storageBucket: "atwarpms.firebasestorage.app",
-  messagingSenderId: "352965627186",
-  appId: "1:352965627186:web:4da393db672e3d90c45b2b",
-  measurementId: "G-6L59HHPPJW"
-};
+// Retired in ATWAR ONE Production 1.7E.
+// Runtime authentication and data access use Supabase only.
